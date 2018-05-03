dd5eec1197bc0040a3abc39dd7e40a0860a28af3
Marcin Świrek <swirek.marcin@gmail.com>
2018-05-03 13:18:48 +0200

    Add some RWD funcionality

--- js/script.js
+++ js/script.js
@@ -1,16 +1,1 @@
-// $(function() {
-//   var $carouselList = $("#carousel ul");
 
-//   setInterval(changeSlides, 5000);
-
-//   function changeSlides() {
-//     $carouselList.animate({ marginLeft: -1900 }, 2000, moveFirstSlide);
-//   }
-
-//   function moveFirstSlide() {
-//     var $firstItem = $carouselList.find("li:first");
-//     var $lastItem = $carouselList.find("li:last");
-//     $lastItem.after($firstItem);
-//     $carouselList.css({ marginLeft: 0 });
-//   }
-// });
