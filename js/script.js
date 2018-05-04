0bae56b10ebf526f46acb417a6aa82f6ce433122
Marcin Swirek <swirek.marcin@gmail.com>
2018-05-04 22:19:35 +0200

    Add rsponsive menu and RWD functionality

--- js/script.js
+++ js/script.js
@@ -1,1 +1,8 @@
-
+function toggleMenu() {
+  var menuList = document.getElementById("navList");
+  if (menuList.className === "menu-list") {
+    menuList.className += " responsive";
+  } else {
+    menuList.className = "menu-list";
+  }
+}
