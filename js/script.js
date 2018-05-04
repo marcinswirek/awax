b9ff5c1937faf145055f6c66669a316939ca3d10
Marcin Swirek <swirek.marcin@gmail.com>
2018-05-04 22:21:25 +0200

    Clear commented parts of code in all files

--- js/script.js
+++ js/script.js
@@ -1,8 +1,1 @@
-function toggleMenu() {
-  var menuList = document.getElementById("navList");
-  if (menuList.className === "menu-list") {
-    menuList.className += " responsive";
-  } else {
-    menuList.className = "menu-list";
-  }
-}
+
